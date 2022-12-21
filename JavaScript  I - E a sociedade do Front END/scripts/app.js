6470b6087ebcbe39a524d2ef9e90b1bafbd1732e
Rute Farias <rute.farias@ccc.ufcg.edu.br>
2022-12-21 14:15:43 -0200

    Function

--- JavaScript  I - E a sociedade do Front END/scripts/app.js
+++ JavaScript  I - E a sociedade do Front END/scripts/app.js
@@ -1,45 +1,45 @@
 //Elabore um script que leia dois números e imprima  o resultado das operações aritméticas soma, 
 //produto, divisão e resto da divisão (%) entre esses números.
 
-//* Tirar dúvida do prompt que concatena os valores ao invés de somar.
-var num1 = 100;
-var num2 = 50;
 
-var soma = num1 + num2;
-var produto = num1 * num2;
-var divisao = num1 / num2;
-var mod = num1 % num2;
+// var num1 = 100;
+// var num2 = 50;
 
-console.log(soma, produto, divisao, mod);
+// var soma = num1 + num2;
+// var produto = num1 * num2;
+// var divisao = num1 / num2;
+// var mod = num1 % num2;
 
-
-//Dado o tamanho do lado de um quadrado, calcular a área e o perímetro do mesmo.
-
-var ladoQuadrado = 5;
-
-var areaQuadrado = ladoQuadrado * ladoQuadrado;
-var perimetroQuadrado = ladoQuadrado * 4;
-
-console.log(areaQuadrado, perimetroQuadrado);
+// console.log(soma, produto, divisao, mod);
 
 
-//Dado que a fórmula para conversão de Fahrenheit para Celsius é C = 5/9 (F – 32), 
-// leu um valor de temperatura em Fahrenheit e exibi-lo em Celsius
+// //Dado o tamanho do lado de um quadrado, calcular a área e o perímetro do mesmo.
 
-var temperaturaFahrenheit = 212;
-var temperaturaCelsius = (temperaturaFahrenheit - 32.0) / 1.8;
-console.log(temperaturaCelsius);
+// var ladoQuadrado = 5;
 
+// var areaQuadrado = ladoQuadrado * ladoQuadrado;
+// var perimetroQuadrado = ladoQuadrado * 4;
 
-// Faça um algoritmo que transforme a velocidade fornecida em m/s pelo usuário para Km/h. Para tal, multiplique o valor em m/s por 3,6.
-
-var velocidadeMs = 100;
-
-var velocidadeKm = velocidadeMs * 3.6;
-console.log(velocidadeKm);
+// console.log(areaQuadrado, perimetroQuadrado);
 
 
-// Crie um object array com os valores de um veículo, sendo as chaves são marca, modelo, cor, ano, valor, 
+// //Dado que a fórmula para conversão de Fahrenheit para Celsius é C = 5/9 (F – 32), 
+// // leu um valor de temperatura em Fahrenheit e exibi-lo em Celsius
+
+// var temperaturaFahrenheit = 212;
+// var temperaturaCelsius = (temperaturaFahrenheit - 32.0) / 1.8;
+// console.log(temperaturaCelsius);
+
+
+// // Faça um algoritmo que transforme a velocidade fornecida em m/s pelo usuário para Km/h. Para tal, multiplique o valor em m/s por 3,6.
+
+// var velocidadeMs = 100;
+
+// var velocidadeKm = velocidadeMs * 3.6;
+// console.log(velocidadeKm);
+
+
+//Crie um object array com os valores de um veículo, sendo as chaves são marca, modelo, cor, ano, valor, 
 // renavam, placa, cada um das chaves devem possuir valores. Exiba através da body do HTML as informações do veículo:
 
 //Veiculo
@@ -50,7 +50,7 @@ console.log(velocidadeKm);
 //renavam: x
 //placa: x
 
-/*
+
 var veiculo = {
     marca: 'Renault',
     modelo: 'Kwid',
@@ -62,82 +62,81 @@ var veiculo = {
 }
 
 
-document.getElementById('marca').innerHTML = veiculo.marca;
-document.getElementById('modelo').innerHTML = veiculo.modelo;
-document.getElementById('cor').innerHTML = veiculo.cor;
-document.getElementById('ano').innerHTML = veiculo.ano;
-document.getElementById('valor').innerHTML = veiculo.valor;
-document.getElementById('renavam').innerHTML = veiculo.renavam;
-document.getElementById('placa').innerHTML = veiculo.placa;
-
-*/
-
-//Crie um array com valores de 10 alunos de sua turma, exiba os valores existentes nas posições: 0,5,2,6,9 e 7.
-
-var arr = ['Bruno', 'Leandro', 'Jana', 'Emilly', 'Marjory', 'Elize', 'Beta', 'Paty', 'Denise', 'Ramiro'];
-
-console.log(arr[0], arr[5], arr[2], arr[6], arr[9], arr[7]);
+document.getElementById('marca').innerHTML = '<p>Marca: </p>' + veiculo.marca;
+document.getElementById('modelo').innerHTML = '<p>Modelo: </p>' + veiculo.modelo;
+document.getElementById('cor').innerHTML = '<p>Cor: </p>' + veiculo.cor;
+document.getElementById('ano').innerHTML = '<p>Ano: </p>' + veiculo.ano;
+document.getElementById('valor').innerHTML = '<p>Valor: </p>' + veiculo.valor;
+document.getElementById('renavam').innerHTML = '<p>Renavam: </p>' + veiculo.renavam;
+document.getElementById('placa').innerHTML = '<p>Placa: </p>' + veiculo.placa;
 
 
-//Crie um array com 5 posições e em cada posição, um object array com as seguintes chave/valor 
-//(id, nome, telefone, email). Exiba o array via console. e acesse as informações do object na posição 3.
+// //Crie um array com valores de 10 alunos de sua turma, exiba os valores existentes nas posições: 0,5,2,6,9 e 7.
+
+// var arr = ['Bruno', 'Leandro', 'Jana', 'Emilly', 'Marjory', 'Elize', 'Beta', 'Paty', 'Denise', 'Ramiro'];
+
+// console.log(arr[0], arr[5], arr[2], arr[6], arr[9], arr[7]);
 
 
-var array = [
-    pos0 = {
-        id: 001,
-        nome: 'Farias',
-        telefone: '7654-9876',
-        email: 'rutefarias@gmail.com'
-
-    },
-
-    pos1 = {
-        id: 002,
-        nome: 'Almeida',
-        telefone: '6578-9876',
-        email: 'preta.almeida@gmail.com'
-    },
-
-    pos2 = {
-        id: 002,
-        nome: 'Almeida',
-        telefone: '6578-9876',
-        email: 'preta.almeida@gmail.com'
-    },
-
-    pos3 = {
-        id: 003,
-        nome: 'Albuquerque',
-        telefone: '6578-8764',
-        email: 'albuquerque@gmail.com'
-    },
-
-    pos4 = {
-        id: 004,
-        nome: 'Magalhães',
-        telefone: '6578-5436',
-        email: 'magal@gmail.com'
-    }
-
-];
-
-console.log(array[3]);
+// //Crie um array com 5 posições e em cada posição, um object array com as seguintes chave/valor 
+// //(id, nome, telefone, email). Exiba o array via console. e acesse as informações do object na posição 3.
 
 
-//8- Um novo modelo de carro da Tesal, super econômico foi lançado.
-//Ele faz 26km com 1 litro de combustível.
-//Cada litro de combustível custa R$ 6,29.
+// var array = [
+//     {
+//         id: 001,
+//         nome: 'Farias',
+//         telefone: '7654-9876',
+//         email: 'rutefarias@gmail.com'
 
-//Faça um programa que pergunte ao usuário quanto de dinheiro ele tem e em seguida diga quantos litros de combustível 
-//ele pode comprar e quantos kilometros o carro consegue andar com este tanto de combustível.
+//     },
+
+//     {
+//         id: 002,
+//         nome: 'Almeida',
+//         telefone: '6578-9876',
+//         email: 'preta.almeida@gmail.com'
+//     },
+
+//     {
+//         id: 002,
+//         nome: 'Almeida',
+//         telefone: '6578-9876',
+//         email: 'preta.almeida@gmail.com'
+//     },
+
+//     {
+//         id: 003,
+//         nome: 'Albuquerque',
+//         telefone: '6578-8764',
+//         email: 'albuquerque@gmail.com'
+//     },
+
+//     {
+//         id: 004,
+//         nome: 'Magalhães',
+//         telefone: '6578-5436',
+//         email: 'magal@gmail.com'
+//     }
+
+// ];
+
+// console.log(array[3]);
 
 
-var dinheiro = prompt('Informe o valor: ');
-var precoLitro = 6.29;
+// //8- Um novo modelo de carro da Tesal, super econômico foi lançado.
+// //Ele faz 26km com 1 litro de combustível.
+// //Cada litro de combustível custa R$ 6,29.
 
-var litros = dinheiro / precoLitro;
-var kms = litros * 26;
+// //Faça um programa que pergunte ao usuário quanto de dinheiro ele tem e em seguida diga quantos litros de combustível 
+// //ele pode comprar e quantos kilometros o carro consegue andar com este tanto de combustível.
 
-console.log('Você pode comprar ' + litros + 'litros.');
-console.log('E o carro consegue fazer ' + kms);
+
+// var dinheiro = prompt('Informe o valor: ');
+// var precoLitro = 6.29;
+
+// var litros = dinheiro / precoLitro;
+// var kms = litros * 26;
+
+// console.log('Você pode comprar ' + litros + 'litros.');
+// console.log('E o carro consegue fazer ' + kms);
