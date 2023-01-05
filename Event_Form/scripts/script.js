f1df7a45263fe530d8caa085a8a72426318c0ba4
Rute Farias <rute.farias@ccc.ufcg.edu.br>
2023-01-05 01:31:56 -0200

    Event and Form

---
+++ Event_Form/scripts/script.js
@@ -0,0 +1,37 @@
+function clicou(){
+    alert('O botão foi clicado');
+}
+
+
+function maior(){
+    let num1 = parseInt(document.getElementById('num1').value);
+    let num2 = parseInt(document.getElementById('num2').value);
+    let num3 = parseInt(document.getElementById('num3').value);
+
+    let result = '';
+
+    if( num1 > num2 && num1 > num3){
+        result = num1;
+    }else if(num2 > num1 && num2 > num3){
+        result = num2;
+    }else{
+        result = num3;
+    }
+
+    alert(result);
+
+    
+}
+function maiorIdade(){
+    let idade = parseInt(document.getElementById('idade').value);
+    let msg = '';
+
+    if(idade <= 18){
+        msg = 'Menor de idade';
+
+    }else{
+        msg = 'Maior de idade';
+    }
+
+    alert(msg);
+}
